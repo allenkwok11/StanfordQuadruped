4b734c989ef9a4cd61fed0e820a3967156d3ea7b
allenkwok11 <allenkwok11@users.noreply.github.com>
2026-03-27 13:17:56 +0800

    Add Ubuntu 22/24 install compatibility

--- install.sh
+++ install.sh
@@ -16,6 +16,19 @@ BASEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ### Append to release file
 echo STANFORD_VERSION=\"$(cd $BASEDIR; ~/mini_pupper_bsp/get-version.sh)\" >> ~/mini-pupper-release
 
+ensure_repo() {
+    local repo_url="$1"
+    local target_dir="$2"
+
+    if [ -d "$target_dir/.git" ]; then
+        git -C "$target_dir" pull --ff-only || true
+    elif [ -d "$target_dir" ]; then
+        echo "Warning: $target_dir exists and is not a git repository, skipping clone"
+    else
+        git clone "$repo_url" "$target_dir"
+    fi
+}
+
 source  ~/mini-pupper-release
 if [ "$IS_RELEASE" == "YES" ]
 then
@@ -28,29 +41,36 @@ then
     fi
 fi
 
-sudo apt-get install -y libatlas-base-dev
-sudo pip3 install numpy transforms3d pyserial
-sudo pip install numpy transforms3d pyserial
-sudo apt-get install -y unzip
+sudo apt-get install -y libatlas-base-dev unzip python3-pip
+
+PY_PKGS="numpy transforms3d pyserial"
+if [[ "$UBUNTU_CODENAME" == "noble" ]]
+then
+    # Ubuntu 24.04 enables PEP 668, so system pip writes require this flag.
+    sudo python3 -m pip install --break-system-packages $PY_PKGS
+else
+    sudo python3 -m pip install $PY_PKGS
+fi
 
 # add bridge to network configuration
 $BASEDIR/configure_network.sh
 # reconfigure network each time network configuration has changed
-echo $BASEDIR/configure_network.sh >> /home/ubuntu/mini_pupper_bsp/System/check-reconfigure.sh
+grep -qxF "$BASEDIR/configure_network.sh" /home/ubuntu/mini_pupper_bsp/System/check-reconfigure.sh || \
+    echo "$BASEDIR/configure_network.sh" >> /home/ubuntu/mini_pupper_bsp/System/check-reconfigure.sh
 
 cd ~
-git clone https://github.com/stanfordroboticsclub/PupperCommand.git
+ensure_repo https://github.com/stanfordroboticsclub/PupperCommand.git ~/PupperCommand
 cd PupperCommand
 sed -i "s/pi/ubuntu/" joystick.service
 sudo bash install.sh
 
 cd ~
-git clone https://github.com/stanfordroboticsclub/UDPComms.git
+ensure_repo https://github.com/stanfordroboticsclub/UDPComms.git ~/UDPComms
 cd UDPComms
 sudo bash install.sh
 
 cd ~
-git clone https://github.com/stanfordroboticsclub/PS4Joystick.git
+ensure_repo https://github.com/stanfordroboticsclub/PS4Joystick.git ~/PS4Joystick
 cd PS4Joystick
 sed -i "s/pi/ubuntu/" joystick.service
 sudo bash install.sh
@@ -59,13 +79,17 @@ cd ~
 sudo systemctl enable joystick
 
 cd ~/StanfordQuadruped
-sudo ln -s $(realpath .)/robot.service /etc/systemd/system/
+sudo ln -sf "$(realpath ./robot.service)" /etc/systemd/system/robot.service
 sudo systemctl daemon-reload
 sudo systemctl enable robot
 sudo systemctl start robot
 
-sudo mv restart_joy.service /lib/systemd/system/
-sudo mv joystart.sh /sbin/
+if [ -f restart_joy.service ]; then
+    sudo install -m 644 restart_joy.service /lib/systemd/system/restart_joy.service
+fi
+if [ -f joystart.sh ]; then
+    sudo install -m 755 joystart.sh /sbin/joystart.sh
+fi
 sudo systemctl enable restart_joy
 source  ~/mini-pupper-release
 if [ "$MACHINE" == "x86_64" ]
